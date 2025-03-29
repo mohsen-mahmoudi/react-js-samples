67f083dbd799ebc0f3a1dc0d92c5a9e051deff77
mohsen-mahmoudi <mohsen.mahmoudi.91@gmail.com>
2025-03-29 10:40:47 +0330

    add new hook for auth

---
+++ part2/1-test-some-functionality/src/part5/5-context/hooks/useAuth.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import RequestClient from '../tools/RequestClient';
+import { removeToken, setToken } from '../tools/Token';
+
+export default function useAuth() {
+    const [user, setUser] = useState()
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoading, setIsLoading] = useState(false)
+
+    function login() {
+        setIsLoading(true)
+        RequestClient('/api/login', { method: 'POST', data: { username: 'mohsen', password: '123' } })
+            .then(response => {
+                // handle token in front
+                setToken(response.data.token)
+                getUser()
+            })
+            .catch(error => {
+                alert(`${error.response.data}`);
+            })
+        //.finally(() => setIsLoading(false))
+    }
+
+    function getUser() {
+        RequestClient('/api/user'/*, { headers: { token: getToken() } } */)
+            .then(response => {
+                setUser(response.data)
+                setIsLoggedIn(true)
+            })
+            .catch(error => {
+                alert(`${error.response.data}`);
+            })
+            .finally(() => setIsLoading(false))
+    }
+
+    function logout() {
+        setUser({})
+        setIsLoggedIn(false)
+        removeToken()
+    }
+
+
+    return ({ user, login, isLoading, isLoggedIn, logout, getUser })
+}
--- part2/1-test-some-functionality/src/part5/5-context/providers/AuthProvider.jsx
+++ part2/1-test-some-functionality/src/part5/5-context/providers/AuthProvider.jsx
@@ -1,46 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import ThemeContext, { themes } from '../contexts/ThemeContext';
 import UserContext from '../contexts/UserContext';
-import RequestClient from '../tools/RequestClient';
-import { getToken, removeToken, setToken } from '../tools/Token';
+import { getToken } from '../tools/Token';
+import useAuth from '../hooks/useAuth';
 
 export default function AuthProvider({ children }) {
 
-    const [user, setUser] = useState()
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [isLoading, setIsLoading] = useState(false)
-
-    function login() {
-        setIsLoading(true)
-        RequestClient('/api/login', { method: 'POST', data: { username: 'mohsen', password: '123' } })
-            .then(response => {
-                // handle token in front
-                setToken(response.data.token)
-                getUser()
-            })
-            .catch(error => {
-                alert(`${error.response.data}`);
-            })
-        //.finally(() => setIsLoading(false))
-    }
-
-    function getUser() {
-        RequestClient('/api/user'/*, { headers: { token: getToken() } } */)
-            .then(response => {
-                setUser(response.data)
-                setIsLoggedIn(true)
-            })
-            .catch(error => {
-                alert(`${error.response.data}`);
-            })
-            .finally(() => setIsLoading(false))
-    }
-
-    function logout() {
-        setUser({})
-        setIsLoggedIn(false)
-        removeToken()
-    }
+    const { user, login, isLoading, isLoggedIn, logout, getUser } = useAuth()
 
     useEffect(() => {
         if (getToken())
